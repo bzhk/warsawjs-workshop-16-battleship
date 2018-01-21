43f2756f6c38bcd3218b4519d0a936586bd84819
bzhk <bzhk911@gmail.com>
2018-01-21 17:30:01 +0100

    full game but not true MVC

--- js/main.js
+++ js/main.js
@@ -12,61 +12,165 @@ class ViewComponent {
 
 class GameCell extends ViewComponent{
 
-  constructor() {
+  constructor(x, y, handleCellClick) {
     super();
     const self = this;
     this._state = 'unknown';
+    this._cords = {x: x, y: y};
     this._element = document.createElement('td');
     this._element.addEventListener('click', function(){
-      self.setState('miss');
-      console.log(self._state)
+      handleCellClick(x, y, self._state);
     });
 
   }
 
   setState(state) {
-    if( state !== 'unknkown' && state !== 'miss' && state !== 'hit'){
+    if( state !== 'unknown' && state !== 'miss' && state !== 'hit' && state !== 'shipField'){
       throw new Error('Invalid state');
     }
     this._state = state;
-    this._element.className = 'cell_' + state;
+    if(state !== 'shipField'){
+      this._element.className = 'cell_' + state;
+    }
   }
 }
 
 class GameBoard extends ViewComponent{
-  constructor(rowsCount, itemsCount){
+  constructor(rowsCount, itemsCount, handleCellClick){
     super();
     this._rowsCount = rowsCount;
     this._itemsCount = itemsCount;
     this.board = [];
-
+    this.ships = [ {x:1,y:1} ]
     this._element = this.buildAllBoard(10,10);
-    console.log(this.board);
+    this.handleCellClick = handleCellClick;
+    this.setShips();
   }
 
-  buildRow(x, y){
-    let row = [];
+  setShips(){
+    this.setStateAt(1,1,'shipField');
+    this.setStateAt(2,1,'shipField');
+    this.setStateAt(3,1,'shipField');
+  }
+
+  buildRow(itemCount, row){
+    let rowArr = [];
     const tr = document.createElement('tr');
-    for(let i=0; i<x; i++){
-      let cell = new GameCell();
-      cell.id = `${i}${y}`;
+    for(let count=0; count<itemCount; count++){
+      let cell = new GameCell(count , row, handleCellClick);
+      cell.getElement().id = `${count}${row}`;
       tr.appendChild(cell.getElement());
-      row.push(cell);
+      rowArr.push(cell);
     }
-    return {row: row, tr: tr,};
+    return {row: rowArr, tr: tr,};
   }
 
-  buildAllBoard(x,y){
+  buildAllBoard(rowsCount, itemsCount){
     const container = document.createElement('table');
-    for(let i=0; i<x; i++){
-      const line = this.buildRow(y, i);
+    for(let i=0; i<rowsCount; i++){
+      const line = this.buildRow(itemsCount, i);
       container.appendChild(line.tr);
       this.board.push(line.row);
     }
     return container;
   }
+
+  setStateAt(row, column, state){
+    return this.board[row][column].setState(state);
+  }
+
+  returnBoard(){
+    return this.board;
+  }
 }
 
-const newGame = new GameBoard(10,10);
+class GameController {
+  constructor(board, gameLoop){
+
+    this._boardView = board;
+  }
+
+  handleCellClick(row, column, state){
+
+    gameLoop.fireAt(row, column, state, this._boardView);
+
+  }
+}
+
+class GameModel {
+    constructor(){
+      this._score = 0;
+      this._rocket = 0;
+      this._lastScore = 0;
+      this._ships = [];
+    }
+
+    fireAt(row, column, state, board){
+      let brake;
+      this._ships.filter( x => {
+        if(x.cords === row + '/' + column){
+          alert('Nie mozesz strzelać w to samo pole');
+          brake = true;
+        }
+      });
+
+      if(brake) {
+        return;
+      }
+
+      let elem = {
+        cords: row + '/' + column,
+        clicked: true,
+        isShip: false,
+      }
+
+      if(state === 'shipField'){
+        this.updateRockets();
+        this.updateScore();
+        this.updateLastScore();
+        elem.isShip = true;
+        board[column][row].setState('hit');
+      }else{
+        this.updateLastScore(true);
+        board[column][row].setState('miss');
+      }
+      this._ships.push(elem);
+      console.log(this._ships)
+    }
+
+
+    updateScore(){
+      this._score += 10;
+      this._lastScore += 10;
+    }
+
+    updateRockets(){
+      this._rocket += 1;
+    }
+
+    updateLastScore(x){
+      if(x){
+        this._lastScore = this._lastScore - 5;
+      }
+    }
+
+    returnScore(){
+      console.log(this._lastScore);
+      return this._lastScore;
+    }
+
+}
+
+let board;
+let controller;
+let gameLoop = new GameModel();
+
+function handleCellClick(row, column, state){
+  controller.handleCellClick(row, column, state);
+}
+
+
+newGame = new GameBoard(10,10, handleCellClick);
+controller = new GameController(newGame.returnBoard(),gameLoop);
 const game = document.getElementById('app');
 game.appendChild(newGame.getElement());
